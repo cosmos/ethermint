0ed710a45ff98b8a6da02dabb1e87161a43b2962
austinabell <austinabell8@gmail.com>
2019-11-04 16:00:21 -0500

    Remove cosmos keys commands

--- cmd/emintcli/main.go
+++ cmd/emintcli/main.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"path"
 
-	"github.com/cosmos/cosmos-sdk/client/keys"
 	"github.com/cosmos/ethermint/rpc"
 	"github.com/tendermint/go-amino"
 
@@ -58,8 +57,6 @@ func main() {
 		// TODO: Set up rest routes (if included, different from web3 api)
 		rpc.Web3RpcCmd(cdc),
 		client.LineBreak,
-		// TODO: Remove these commands once ethermint keys and genesis set up
-		keys.Commands(),
 		emintkeys.Commands(),
 		client.LineBreak,
 	)
