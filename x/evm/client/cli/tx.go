292b33d351fe3d92322683bdbffba7a541ad11c8
austinabell <austinabell8@gmail.com>
2019-11-12 16:43:19 -0500

    Change transaction command to not include create operation (to include other command in next commit)

--- x/evm/client/cli/tx.go
+++ x/evm/client/cli/tx.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
@@ -18,10 +17,6 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	emint "github.com/cosmos/ethermint/types"
 	"github.com/cosmos/ethermint/x/evm/types"
-)
-
-const (
-	flagToAddress = "to-address"
 )
 
 // GetTxCmd defines the CLI commands regarding evm module transactions
@@ -41,40 +36,35 @@ func GetTxCmd(storeKey string, cdc *codec.Codec) *cobra.Command {
 	return evmTxCmd
 }
 
-// GetCmdGenTx generates an ethereum transaction wrapped in a Cosmos standard transaction
+// GetCmdGenTx generates an Emint transaction (excludes create operations)
 func GetCmdGenTx(cdc *codec.Codec) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "generate-tx [amount (in photons)] [<data>]",
-		Short: "generate eth tx wrapped in a Cosmos Standard tx",
-		Args:  cobra.RangeArgs(1, 2),
+	return &cobra.Command{
+		Use:   "send [to_address] [amount (in photons)] [<data>]",
+		Short: "send transaction to address (call operations included)",
+		Args:  cobra.RangeArgs(2, 3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 
 			// Ambiguously decode amount from any base
-			amount, err := strconv.ParseInt(args[0], 0, 64)
+			amount, err := strconv.ParseInt(args[1], 0, 64)
 			if err != nil {
 				return err
 			}
 
 			var data []byte
-			if len(args) > 1 {
-				payload := args[1]
+			if len(args) > 2 {
+				payload := args[2]
 				data, err = hexutil.Decode(payload)
 				if err != nil {
 					fmt.Println(err)
 				}
 			}
 
-			var toAddr *sdk.AccAddress
-			toFlag := viper.GetString(flagToAddress)
-			if toFlag != "" {
-				addr, err := sdk.AccAddressFromBech32(toFlag)
-				if err != nil {
-					return errors.Wrap(err, "must provide a valid Bech32 address for ")
-				}
-				toAddr = &addr
+			toAddr, err := sdk.AccAddressFromBech32(args[0])
+			if err != nil {
+				return errors.Wrap(err, "must provide a valid Bech32 address for to_address")
 			}
 
 			from := cliCtx.GetFromAddress()
@@ -84,7 +74,8 @@ func GetCmdGenTx(cdc *codec.Codec) *cobra.Command {
 				return errors.Wrap(err, "Could not retrieve account sequence")
 			}
 
-			msg := types.NewEmintMsg(seq, toAddr, sdk.NewInt(amount), txBldr.Gas(),
+			// TODO: Potentially allow overriding of gas price and gas limit
+			msg := types.NewEmintMsg(seq, &toAddr, sdk.NewInt(amount), txBldr.Gas(),
 				sdk.NewInt(emint.DefaultGasPrice), data, from)
 
 			err = msg.ValidateBasic()
@@ -95,9 +86,4 @@ func GetCmdGenTx(cdc *codec.Codec) *cobra.Command {
 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
 	}
-
-	// Optional parameter flags
-	cmd.Flags().String(flagToAddress, "", "set to address for transaction")
-
-	return cmd
 }
