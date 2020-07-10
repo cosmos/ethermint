ad985440ce6fa34056561964302d5a1c2f8b9252
araskachoi <choidanielw@gmail.com>
2020-07-10 11:55:08 -0700

    bypass nonce in antehandler

--- app/ante/eth.go
+++ app/ante/eth.go
@@ -237,7 +237,7 @@ func (nvd NonceVerificationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, sim
 	}
 
 	seq := acc.GetSequence()
-	if msgEthTx.Data.AccountNonce != seq {
+	if msgEthTx.Data.AccountNonce < seq {
 		return ctx, sdkerrors.Wrap(
 			sdkerrors.ErrInvalidSequence,
 			fmt.Sprintf("invalid nonce; got %d, expected %d", msgEthTx.Data.AccountNonce, seq),
