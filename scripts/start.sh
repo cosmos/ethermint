36cec6b575ad35596cd95a42e672970814888d67
holechain <32643286+holechain@users.noreply.github.com>
2020-08-06 17:55:03 +0800

    fix the 'docker logs -f ethermintdnode0' command

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 ethermintd --home /ethermint/node$ID/ethermintd/ start > ethermintd.log &
 sleep 5
 ethermintcli rest-server --laddr "tcp://localhost:8545" --chain-id 7305661614933169792 --trace > ethermintcli.log &
-tail -f /dev/null
+tail -f ethermintd.log
