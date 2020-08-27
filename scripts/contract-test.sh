221c8d511e31184e9fc8806dbf163bfe63155831
araskachoi <choidanielw@gmail.com>
2020-08-27 10:24:15 -0700

    update binary names

--- scripts/contract-test.sh
+++ scripts/contract-test.sh
@@ -7,48 +7,47 @@ CHAINID=123
 MONIKER="localbenchmarktestnet"
 
 # remove existing daemon and client
-rm -rf ~/.emint*
-pkill -f "emint*"
+pkill -f "ethermint*"
 
-type "emintd" 2> /dev/null || make install
-type "emintcli" 2> /dev/null || make install
+type "ethermintd" 2> /dev/null || make install
+type "ethermintcli" 2> /dev/null || make install
 
 emintcli config keyring-backend test
 
 # Set up config for CLI
-emintcli config chain-id $CHAINID
-emintcli config output json
-emintcli config indent true
-emintcli config trust-node true
+ethermintcli config chain-id $CHAINID
+ethermintcli config output json
+ethermintcli config indent true
+ethermintcli config trust-node true
 
 # if $KEY exists it should be deleted
-emintcli keys add $FKEY
+ethermintcli keys add $FKEY
 
 # Set moniker and chain-id for Ethermint (Moniker can be anything, chain-id must be an integer)
-emintd init $MONIKER --chain-id $CHAINID
+ethermintcli init $MONIKER --chain-id $CHAINID
 
 # Use a custom genesis with pre-generated keys
-cp ./benchmark_ethmint_genesis.json $HOME/.emintd/config/genesis.json
+cp ./benchmark_ethmint_genesis.json $HOME/.ethermintd/config/genesis.json
 
 # Allocate genesis accounts (cosmos formatted addresses)
-emintd add-genesis-account $(emintcli keys show $FKEY -a) 1000000000000000000photon,1000000000000000000stake
+ethermintd add-genesis-account $(emintcli keys show $FKEY -a) 1000000000000000000photon,1000000000000000000stake
 
 # Sign genesis transaction
-emintd gentx --name $FKEY --keyring-backend test
+ethermintd gentx --name $FKEY --keyring-backend test
 
 # Collect genesis tx
-emintd collect-gentxs
+ethermintd collect-gentxs
 
 # Run this to ensure everything worked and that the genesis file is setup correctly
-emintd validate-genesis
+ethermintd validate-genesis
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed) in background and log to emintd.log
-emintd start --pruning=nothing --rpc.unsafe --log_level "main:info,state:info,mempool:info" --trace > emintd.log &
+ethermintd start --pruning=nothing --rpc.unsafe --log_level "main:info,state:info,mempool:info" --trace > ethermintd.log &
 
 sleep 1
 
 # Start the rest server with unlocked faucet key in background and log to emintcli.log 
-emintcli rest-server --laddr "tcp://localhost:8545" --unlock-key $FKEY --chain-id $CHAINID --trace > emintcli.log &
+ethermintcli rest-server --laddr "tcp://localhost:8545" --unlock-key $FKEY --chain-id $CHAINID --trace > ethermintcli.log &
 
 solc --abi contracts/counter/counter.sol --bin -o contracts/counter
 mv contracts/counter/contracts_counter_counter_sol_Counter.abi contracts/counter/counter_sol.abi
