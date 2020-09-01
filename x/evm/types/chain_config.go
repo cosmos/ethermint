c7c89ab368ca99bde9d843703bf665b9280f68a7
Federico Kunze <federico.kunze94@gmail.com>
2020-09-01 17:22:56 +0200

    additional test

--- x/evm/types/chain_config.go
+++ x/evm/types/chain_config.go
@@ -15,32 +15,33 @@ import (
 
 // ChainConfig defines the Ethereum ChainConfig parameters using sdk.Int values instead of big.Int.
 //
-// NOTE 1: Since empty Int (i.e with a nil big.Int value) are parsed to zero, we need to manually
-// specify
+// NOTE 1: Since empty/uninitialized Ints (i.e with a nil big.Int value) are parsed to zero, we need to manually
+// specify that negative Int values will be considered as nil. See getBlockValue for reference.
+//
 // NOTE 2: This type is not a configurable Param since the SDK does not allow for validation against
-// previous stored parameter values or current block height. In order to update the values, a
-// hard-fork upgrade must be done.
+// a previous stored parameter values or the current block height (retrieved from context). If you
+// want to update the config values, use an software upgrade procedure.
 type ChainConfig struct {
-	HomesteadBlock sdk.Int `json:"homestead_block" yaml:"homestead_block"` // Homestead switch block (nil = no fork, 0 < already homestead)
+	HomesteadBlock sdk.Int `json:"homestead_block" yaml:"homestead_block"` // Homestead switch block (< 0 no fork, 0 = already homestead)
 
-	DAOForkBlock   sdk.Int `json:"dao_fork_block" yaml:"dao_fork_block"`     // TheDAO hard-fork switch block (nil = no fork)
+	DAOForkBlock   sdk.Int `json:"dao_fork_block" yaml:"dao_fork_block"`     // TheDAO hard-fork switch block (< 0 no fork)
 	DAOForkSupport bool    `json:"dao_fork_support" yaml:"dao_fork_support"` // Whether the nodes supports or opposes the DAO hard-fork
 
 	// EIP150 implements the Gas price changes (https://github.com/ethereum/EIPs/issues/150)
-	EIP150Block sdk.Int `json:"eip150_block" yaml:"eip150_block"` // EIP150 HF block (nil = no fork)
+	EIP150Block sdk.Int `json:"eip150_block" yaml:"eip150_block"` // EIP150 HF block (< 0 no fork)
 	EIP150Hash  string  `json:"eip150_hash" yaml:"eip150_hash"`   // EIP150 HF hash (needed for header only clients as only gas pricing changed)
 
 	EIP155Block sdk.Int `json:"eip155_block" yaml:"eip155_block"` // EIP155 HF block
 	EIP158Block sdk.Int `json:"eip158_block" yaml:"eip158_block"` // EIP158 HF block
 
-	ByzantiumBlock      sdk.Int `json:"byzantium_block" yaml:"byzantium_block"`           // Byzantium switch block (nil = no fork, 0 < already on byzantium)
-	ConstantinopleBlock sdk.Int `json:"constantinople_block" yaml:"constantinople_block"` // Constantinople switch block (nil = no fork, 0 < already activated)
-	PetersburgBlock     sdk.Int `json:"petersburg_block" yaml:"petersburg_block"`         // Petersburg switch block (nil = same as Constantinople)
-	IstanbulBlock       sdk.Int `json:"istanbul_block" yaml:"istanbul_block"`             // Istanbul switch block (nil = no fork, 0 < already on istanbul)
-	MuirGlacierBlock    sdk.Int `json:"muir_glacier_block" yaml:"muir_glacier_block"`     // Eip-2384 (bomb delay) switch block (nil = no fork, 0 < already activated)
+	ByzantiumBlock      sdk.Int `json:"byzantium_block" yaml:"byzantium_block"`           // Byzantium switch block (< 0 no fork, 0 = already on byzantium)
+	ConstantinopleBlock sdk.Int `json:"constantinople_block" yaml:"constantinople_block"` // Constantinople switch block (< 0 no fork, 0 = already activated)
+	PetersburgBlock     sdk.Int `json:"petersburg_block" yaml:"petersburg_block"`         // Petersburg switch block (< 0 same as Constantinople)
+	IstanbulBlock       sdk.Int `json:"istanbul_block" yaml:"istanbul_block"`             // Istanbul switch block (< 0 no fork, 0 = already on istanbul)
+	MuirGlacierBlock    sdk.Int `json:"muir_glacier_block" yaml:"muir_glacier_block"`     // Eip-2384 (bomb delay) switch block (< 0 no fork, 0 = already activated)
 
 	YoloV1Block sdk.Int `json:"yoloV1_block" yaml:"yoloV1_block"` // YOLO v1: https://github.com/ethereum/EIPs/pull/2657 (Ephemeral testnet)
-	EWASMBlock  sdk.Int `json:"ewasm_block" yaml:"ewasm_block"`   // EWASM switch block (nil = no fork, 0 = already activated)
+	EWASMBlock  sdk.Int `json:"ewasm_block" yaml:"ewasm_block"`   // EWASM switch block (< 0 no fork, 0 = already activated)
 }
 
 // EthereumConfig returns an Ethereum ChainConfig for EVM state transitions.
