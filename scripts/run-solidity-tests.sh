ef6f7d066b4f98d81eaae320ba7e581aff4430f2
noot <elizabethjbinks@gmail.com>
2020-09-10 14:49:09 -0400

    add test-solidity to github actions, create test script

---
+++ scripts/run-solidity-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+make install
+cd tests-solidity
+
+if command -v yarn &> /dev/null; then
+	yarn install
+else 
+	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	sudo apt update && sudo apt install yarn
+	yarn install
+fi
+
+chmod +x ./init-test-node.sh
+./init-test-node.sh > ethermintd.log &
+sleep 5
+ethermintcli rest-server --laddr "tcp://localhost:8545" --unlock-key localkey,user1,user2 --chain-id 1337 --trace --wsport 8546 > ethermintcli.log &
+
+cd suites/initializable
+yarn test-ethermint
+
+ok=$?
+
+killall ethermintcli
+killall ethermintd
+
+echo "Script exited with code $ok"
+exit $ok
