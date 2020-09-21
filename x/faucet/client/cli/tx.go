5aafa8bed4d273d9022e8ce50125599873c1c189
araskachoi <choidanielw@gmail.com>
2020-09-21 16:10:59 -0700

    faucet changes

--- x/faucet/client/cli/tx.go
+++ x/faucet/client/cli/tx.go
@@ -11,10 +11,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/crypto/keys"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	authclient "github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	"github.com/cosmos/cosmos-sdk/crypto/keys"
 
 	"github.com/cosmos/ethermint/crypto"
 	"github.com/cosmos/ethermint/x/faucet/types"
@@ -40,12 +40,12 @@ func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 // GetCmdRequest is the CLI command to fund an address with the requested coins
 func GetCmdRequest(cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "request [amount] [other-recipient (optional)]",
+		Use:   "request [faucet-address] [recipient-address] [amount] ",
 		Short: "request an address with the requested coins",
-		Args:  cobra.RangeArgs(1, 2),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			inBuf := bufio.NewReader(cmd.InOrStdin())
-			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			cliCtx := context.NewCLIContextWithInputAndFrom(inBuf, args[0]).WithCodec(cdc)
 			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(authclient.GetTxEncoder(cdc))
 
 			var err error
@@ -61,39 +61,79 @@ func GetCmdRequest(cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			amount, err := sdk.ParseCoins(args[0])
+			fmt.Println("-- Keyring! -- ")
+
+			infos, err := cliCtx.Keybase.List()
 			if err != nil {
 				return err
 			}
 
-			var recipient sdk.AccAddress
-			if len(args) == 1 {
-				recipient = cliCtx.GetFromAddress()
+			for _, info := range infos {
+				cmd.Println("name: ", info.GetName())
+				fmt.Println("pubkey: ", info.GetPubKey())
+				fmt.Println("addr: ", info.GetAddress().String())
+				path, err := info.GetPath()
+				fmt.Println("path: ", path)
+				fmt.Println("err? ", err)
+			}
+
+			var faucet, recipient sdk.AccAddress
+
+			// if len(args[0]) < 42 {
+			// 	for _, info := range infos {
+			// 		if args[0] == info.GetName() {
+			// 			faucet = info.GetAddress()
+			// 		}
+			// 	}
+			// } else {
+			// 	faucet, err = sdk.AccAddressFromBech32(args[0])
+			// 	if err != nil {
+			// 		return err
+			// 	}
+			// }
+
+			if len(args[1]) < 42 {
+				for _, info := range infos {
+					if args[1] == info.GetName() {
+						recipient = info.GetAddress()
+					}
+				}
 			} else {
 				recipient, err = sdk.AccAddressFromBech32(args[1])
+				if err != nil {
+					return err
+				}
 			}
 
+			amount, err := sdk.ParseCoins(args[2])
 			if err != nil {
-				fmt.Println("failed to create acc address")
 				return err
 			}
 
-			msg := types.NewMsgFund(amount, cliCtx.GetFromAddress(), recipient)
+			fmt.Println("recipient: ", recipient)
+
+			fmt.Println("faucet addr: ", faucet)
+
+			accRet := auth.NewAccountRetriever(cliCtx)
+			fmt.Println("accRet: ", accRet)
+
+			err = accRet.EnsureExists(faucet)
+			if err != nil {
+				return fmt.Errorf("faucet account does not exist: %s", faucet)
+			}
+
+			msg := types.NewMsgFund(amount, faucet, recipient)
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
 
-			accRet := auth.NewAccountRetriever(cliCtx)
-			err = accRet.EnsureExists(recipient)
-			if err != nil {
-				// account doesn't exist
-				return fmt.Errorf("nonexistent account %s: %s", recipient, err)
-			}
+			fmt.Println("msg:", msg)
 
 			err = authclient.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 			if err != nil {
 				fmt.Println("failed to run request command:", err)
 			}
+
 			return err
 		},
 	}
