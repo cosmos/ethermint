d6ed856928dc887e968242adaf03ac4d429ff58f
Federico Kunze <federico.kunze94@gmail.com>
2020-10-07 14:26:49 +0200

    revert script changes

--- init.sh
+++ init.sh
@@ -1,175 +1,56 @@
-#!/bin/bash
-
-# "stable" mode tests assume data is static
-# "live" mode tests assume data dynamic
-
-SCRIPT=$(basename ${BASH_SOURCE[0]})
-TEST=""
-QTD=1
-SLEEP_TIMEOUT=5
-TEST_QTD=1
-
-#PORT AND RPC_PORT 3 initial digits, to be concat with a suffix later when node is initialized
-RPC_PORT="854"
-IP_ADDR="0.0.0.0"
-MODE="rpc"
 
 KEY="mykey"
-CHAINID="ethermint-2"
-MONIKER="mymoniker"
+CHAINID="ethermint-1"
+MONIKER="localtestnet"
 
-## default port prefixes for ethermintd
-NODE_P2P_PORT="2660"
-NODE_PORT="2663"
-NODE_RPC_PORT="2666"
+# remove existing daemon and client
+rm -rf ~/.ethermint*
 
-usage() {
-    echo "Usage: $SCRIPT"
-    echo "Optional command line arguments"
-    echo "-t <string>  -- Test to run. eg: rpc"
-    echo "-q <number>  -- Quantity of nodes to run. eg: 3"
-    echo "-z <number>  -- Quantity of nodes to run tests against eg: 3"
-    echo "-s <number>  -- Sleep between operations in secs. eg: 5"
-    exit 1
-}
+make install
 
-while getopts "h?t:q:z:s:" args; do
-    case $args in
-        h|\?)
-            usage;
-        exit;;
-        t ) TEST=${OPTARG};;
-        q ) QTD=${OPTARG};;
-        z ) TEST_QTD=${OPTARG};;
-        s ) SLEEP_TIMEOUT=${OPTARG};;
-    esac
-done
+ethermintd config keyring-backend test
 
-set -euxo pipefail
+# Set up config for CLI
+ethermintd config chain-id $CHAINID
+ethermintd config output json
+ethermintd config indent true
+ethermintd config trust-node true
 
-DATA_DIR=$(mktemp -d -t ethermint-datadir.XXXXX)
+# if $KEY exists it should be deleted
+ethermintd keys add $KEY
 
-if [[ ! "$DATA_DIR" ]]; then
-    echo "Could not create $DATA_DIR"
-    exit 1
-fi
+# Set moniker and chain-id for Ethermint (Moniker can be anything, chain-id must be an integer)
+ethermintd init $MONIKER --chain-id $CHAINID
 
-DATA_CLI_DIR=$(mktemp -d -t ethermint-cli-datadir.XXXXX)
+# Change parameter token denominations to aphoton
+cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aphoton"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
+cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aphoton"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
+cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aphoton"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
+cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="aphoton"' > $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
 
-if [[ ! "$DATA_CLI_DIR" ]]; then
-    echo "Could not create $DATA_CLI_DIR"
-    exit 1
-fi
+# Enable faucet
+cat $HOME/.ethermintd/config/genesis.json | jq '.app_state["faucet"]["enable_faucet"]=true' >  $HOME/.ethermintd/config/tmp_genesis.json && mv $HOME/.ethermintd/config/tmp_genesis.json $HOME/.ethermintd/config/genesis.json
 
-# Compile ethermint
-echo "compiling ethermint"
-make build-ethermint
+# Allocate genesis accounts (cosmos formatted addresses)
+ethermintd add-genesis-account $(ethermintd keys show $KEY -a) 100000000000000000000aphoton
 
-# PID array declaration
-arr=()
+# Sign genesis transaction
+ethermintd gentx --name $KEY --amount=1000000000000000000aphoton --keyring-backend test
 
-# PID arraycli declaration
-arrcli=()
+# Collect genesis tx
+ethermintd collect-gentxs
 
-init_func() {
-    echo "create and add new keys"
-    "$PWD"/build/ethermintd config keyring-backend test --home "$DATA_CLI_DIR$i"
-    "$PWD"/build/ethermintd keys add $KEY"$i" --home "$DATA_CLI_DIR$i" --no-backup --chain-id $CHAINID
-    echo "init Ethermint with moniker=$MONIKER and chain-id=$CHAINID"
-    "$PWD"/build/ethermintd init $MONIKER --chain-id $CHAINID --home "$DATA_DIR$i"
-    echo "init ethermintd with chain-id=$CHAINID and config it trust-node true"
-    "$PWD"/build/ethermintd config chain-id $CHAINID --home "$DATA_CLI_DIR$i"
-    "$PWD"/build/ethermintd config output json --home "$DATA_CLI_DIR$i"
-    "$PWD"/build/ethermintd config indent true --home "$DATA_CLI_DIR$i"
-    "$PWD"/build/ethermintd config trust-node true --home "$DATA_CLI_DIR$i"
-    echo "prepare genesis: Allocate genesis accounts"
-    "$PWD"/build/ethermintd add-genesis-account \
-    "$("$PWD"/build/ethermintd keys show "$KEY$i" -a --home "$DATA_CLI_DIR$i" )" 1000000000000000000aphoton,1000000000000000000stake \
-    --home "$DATA_DIR$i" --home-client "$DATA_CLI_DIR$i"
-    echo "prepare genesis: Sign genesis transaction"
-    "$PWD"/build/ethermintd gentx --name $KEY"$i" --keyring-backend test --home "$DATA_DIR$i" --home-client "$DATA_CLI_DIR$i"
-    echo "prepare genesis: Collect genesis tx"
-    "$PWD"/build/ethermintd collect-gentxs --home "$DATA_DIR$i"
-    echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
-    "$PWD"/build/ethermintd validate-genesis --home "$DATA_DIR$i"
-}
-
-start_func() {
-    echo "starting ethermint node $i in background ..."
-    "$PWD"/build/ethermintd start --pruning=nothing --rpc.unsafe --log_level "main:info,state:info,mempool:info" \
-    --p2p.laddr tcp://$IP_ADDR:$NODE_P2P_PORT"$i" --address tcp://$IP_ADDR:$NODE_PORT"$i" --rpc.laddr tcp://$IP_ADDR:$NODE_RPC_PORT"$i" \
-    --home "$DATA_DIR$i" \
-    >"$DATA_DIR"/node"$i".log 2>&1 & disown
-    
-    ETHERMINT_PID=$!
-    echo "started ethermint node, pid=$ETHERMINT_PID"
-    # add PID to array
-    arr+=("$ETHERMINT_PID")
-}
-
-start_cli_func() {
-    echo "starting ethermint node $i in background ..."
-    "$PWD"/build/ethermintd rest-server --unlock-key $KEY"$i" --chain-id $CHAINID --trace \
-    --laddr "tcp://localhost:$RPC_PORT$i" --node tcp://$IP_ADDR:$NODE_RPC_PORT"$i" \
-    --home "$DATA_CLI_DIR$i" --read-timeout 30 --write-timeout 30 \
-    >"$DATA_CLI_DIR"/cli"$i".log 2>&1 & disown
-    
-    ETHERMINT_CLI_PID=$!
-    echo "started ethermintd node, pid=$ETHERMINT_CLI_PID"
-    # add PID to array
-    arrcli+=("$ETHERMINT_CLI_PID")
-}
-
-# Run node with static blockchain database
-# For loop N times
-for i in $(seq 1 "$QTD"); do
-    init_func "$i"
-    start_func "$i"
-    sleep 1
-    start_cli_func "$i"
-    echo "sleeping $SLEEP_TIMEOUT seconds for startup"
-    sleep "$SLEEP_TIMEOUT"
-    echo "done sleeping"
-done
-
-echo "sleeping $SLEEP_TIMEOUT seconds before running tests ... "
-sleep "$SLEEP_TIMEOUT"
-echo "done sleeping"
-
-set +e
-
-if [[ -z $TEST || $TEST == "rpc" ]]; then
-    
-    for i in $(seq 1 "$TEST_QTD"); do
-        HOST_RPC=http://$IP_ADDR:$RPC_PORT"$i"
-        echo "going to test ethermint node $HOST_RPC ..."
-        MODE=$MODE HOST=$HOST_RPC go test ./tests/... -timeout=300s -v -short
-        
-        RPC_FAIL=$?
-    done
-    
-fi
-
-stop_func() {
-    ETHERMINT_PID=$i
-    echo "shutting down node, pid=$ETHERMINT_PID ..."
-    
-    # Shutdown ethermint node
-    kill -9 "$ETHERMINT_PID"
-    wait "$ETHERMINT_PID"
-}
+echo -e '\n\ntestnet faucet enabled'
+echo -e 'to transfer tokens to your account address use:'
+echo -e "ethermintd tx faucet request 100aphoton --from $KEY\n"
 
 
-for i in "${arrcli[@]}"; do
-    stop_func "$i"
-done
+# Run this to ensure everything worked and that the genesis file is setup correctly
+ethermintd validate-genesis
 
-for i in "${arr[@]}"; do
-    stop_func "$i"
-done
+# Command to run the rest server in a different terminal/window
+echo -e '\nrun the following command in a different terminal/window to run the REST server and JSON-RPC:'
+echo -e "ethermintd rest-server --laddr \"tcp://localhost:8545\" --unlock-key $KEY --chain-id $CHAINID --trace\n"
 
-if [[ (-z $TEST || $TEST == "rpc") && $RPC_FAIL -ne 0 ]]; then
-    exit $RPC_FAIL
-else
-    exit 0
-fi
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+ethermintd start --pruning=nothing --rpc.unsafe --log_level "main:info,state:info,mempool:info" --trace
