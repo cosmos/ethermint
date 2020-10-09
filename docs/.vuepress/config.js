dcaa10c121a2c5999e5178b324e51f8b764833ac
Federico Kunze <federico.kunze94@gmail.com>
2020-10-09 10:58:04 +0200

    ChainSafe/ethermint -> cosmos/ethermint renaming

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -55,11 +55,8 @@ module.exports = {
         bg: 'linear-gradient(221.79deg, #3D6B99 -1.08%, #336699 95.88%)',
         logo: 'ethereum-white'
       },
-      github: {
-        title: 'Found an Issue?',
-        text: 'Help us improve this page by suggesting edits on GitHub.',
-        bg: '#F8F9FC'
-      }
+      github:
+          {title: 'Found an Issue?', text: 'Help us improve this page by suggesting edits on GitHub.', bg: '#F8F9FC'}
     },
     footer: {
       logo: '/logo-bw.svg',
@@ -93,9 +90,9 @@ module.exports = {
           title: 'Contributing',
           children: [
             {title: 'Contributing to the docs', url: 'https://github.com/ChainSafe/ethermint/tree/development/docs'},
-            {title: 'Careers at Chainsafe', url: 'https://chainsafe.io/#careers'}, {
+            {title: 'Careers at ChainSafe', url: 'https://chainsafe.io/#careers'}, {
               title: 'Source code on GitHub',
-              url: 'https://github.com/Chainsafe/ethermint/blob/development/docs/DOCS_README.md'
+              url: 'https://github.com/cosmos/ethermint/blob/development/docs/DOCS_README.md'
             }
           ]
         }
