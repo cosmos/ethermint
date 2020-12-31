7a8528d795aec408c32c80a12ed2ef3a75f285f4
summerpro <974741468@qq.com>
2021-01-01 00:32:43 +0800

    optimize code

--- x/evm/client/rest/rest.go
+++ x/evm/client/rest/rest.go
@@ -4,21 +4,15 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"net/http"
-	"time"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	rpctypes "github.com/cosmos/ethermint/rpc/types"
-	evmtypes "github.com/cosmos/ethermint/x/evm/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gorilla/mux"
-	"github.com/tendermint/tendermint/rpc/client"
-	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
 // RegisterRoutes - Central function to define routes that get registered by the main application
@@ -39,134 +33,62 @@ func QueryTxRequestHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 		if !ok {
 			return
 		}
+
+		ethHashPrefix := "0x"
+
 		var output interface{}
-		output, err := QueryTx(cliCtx, hashHexStr)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
-			return
-
+		var err error
+		if strings.HasPrefix(hashHexStr, ethHashPrefix) {
+			ethHashPrefixLength := len(ethHashPrefix)
+			output, err = getEthTransactionByHash(cliCtx, hashHexStr[ethHashPrefixLength:])
+			if err != nil {
+				rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+		} else {
+			output, err = utils.QueryTx(cliCtx, hashHexStr)
+			if err != nil {
+				if strings.Contains(err.Error(), hashHexStr) {
+					rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
+					return
+				}
+				rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+				return
+			}
 		}
-
 		rest.PostProcessResponseBare(w, cliCtx, output)
 	}
 }
 
-// QueryTx queries for a single transaction by a hash string in hex format. An
-// error is returned if the transaction does not exist or cannot be queried.
-func QueryTx(cliCtx context.CLIContext, hashHexStr string) (interface{}, error) {
-	hash, err := hex.DecodeString(hashHexStr)
-	if err != nil {
-		return sdk.TxResponse{}, err
-	}
-
-	node, err := cliCtx.GetNode()
-	if err != nil {
-		return sdk.TxResponse{}, err
-	}
-
-	resTx, err := node.Tx(hash, !cliCtx.TrustNode)
-	if err != nil {
-		return sdk.TxResponse{}, err
-	}
-
-	if !cliCtx.TrustNode {
-		if err = ValidateTxResult(cliCtx, resTx); err != nil {
-			return sdk.TxResponse{}, err
-		}
-	}
-
-	tx, err := evmtypes.TxDecoder(cliCtx.Codec)(resTx.Tx)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
-	}
-
-	ethTx, ok := tx.(evmtypes.MsgEthereumTx)
-	if ok {
-		return getEthTxResponse(node, resTx, ethTx)
-	}
-	// not eth Tx
-	resBlocks, err := getBlocksForTxResults(cliCtx, []*ctypes.ResultTx{resTx})
-	if err != nil {
-		return sdk.TxResponse{}, err
-	}
-
-	out, err := formatTxResult(cliCtx.Codec, resTx, resBlocks[resTx.Height])
-	if err != nil {
-		return out, err
-	}
-
-	return out, nil
-
-}
-
-func getEthTxResponse(node client.Client, resTx *ctypes.ResultTx, ethTx evmtypes.MsgEthereumTx) (interface{}, error) {
-	// Can either cache or just leave this out if not necessary
-	block, err := node.Block(&resTx.Height)
+// GetTransactionByHash returns the transaction identified by hash.
+func getEthTransactionByHash(cliCtx context.CLIContext, hashHex string) ([]byte, error) {
+	hash, err := hex.DecodeString(hashHex)
 	if err != nil {
 		return nil, err
 	}
+	node, err := cliCtx.GetNode()
+	if err != nil {
+		return nil, err
+	}
+	tx, err := node.Tx(hash, false)
+
+	// Can either cache or just leave this out if not necessary
+	block, err := node.Block(&tx.Height)
+	if err != nil {
+		return nil, err
+	}
+
 	blockHash := common.BytesToHash(block.Block.Header.Hash())
-	height := uint64(resTx.Height)
-	res, err := rpctypes.NewTransaction(&ethTx, common.BytesToHash(resTx.Tx.Hash()), blockHash, height, uint64(resTx.Index))
+
+	ethTx, err := rpctypes.RawTxToEthTx(cliCtx, tx.Tx)
+	if err != nil {
+		return nil, err
+	}
+
+	height := uint64(tx.Height)
+	res, err := rpctypes.NewTransaction(ethTx, common.BytesToHash(tx.Tx.Hash()), blockHash, height, uint64(tx.Index))
 	if err != nil {
 		return nil, err
 	}
 	return json.Marshal(res)
-}
-
-// ValidateTxResult performs transaction verification.
-func ValidateTxResult(cliCtx context.CLIContext, resTx *ctypes.ResultTx) error {
-	if !cliCtx.TrustNode {
-		check, err := cliCtx.Verify(resTx.Height)
-		if err != nil {
-			return err
-		}
-		err = resTx.Proof.Validate(check.Header.DataHash)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func getBlocksForTxResults(cliCtx context.CLIContext, resTxs []*ctypes.ResultTx) (map[int64]*ctypes.ResultBlock, error) {
-	node, err := cliCtx.GetNode()
-	if err != nil {
-		return nil, err
-	}
-
-	resBlocks := make(map[int64]*ctypes.ResultBlock)
-
-	for _, resTx := range resTxs {
-		if _, ok := resBlocks[resTx.Height]; !ok {
-			resBlock, err := node.Block(&resTx.Height)
-			if err != nil {
-				return nil, err
-			}
-
-			resBlocks[resTx.Height] = resBlock
-		}
-	}
-
-	return resBlocks, nil
-}
-
-func formatTxResult(cdc *codec.Codec, resTx *ctypes.ResultTx, resBlock *ctypes.ResultBlock) (sdk.TxResponse, error) {
-	tx, err := parseTx(cdc, resTx.Tx)
-	if err != nil {
-		return sdk.TxResponse{}, err
-	}
-
-	return sdk.NewResponseResultTx(resTx, tx, resBlock.Block.Time.Format(time.RFC3339)), nil
-}
-
-func parseTx(cdc *codec.Codec, txBytes []byte) (sdk.Tx, error) {
-	var tx types.StdTx
-
-	err := cdc.UnmarshalBinaryLengthPrefixed(txBytes, &tx)
-	if err != nil {
-		return nil, err
-	}
-
-	return tx, nil
 }
